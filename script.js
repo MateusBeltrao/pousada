ab0294f2d00c46403a947dc196eb2fded332e9df
mateus beltrão <mateusbeltrao2019@gmail.com>
2024-10-22 08:35:45 -0300

    Primeiro commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// Inicializa o AOS
 AOS.init();
 
+// Menu Toggle
 const menuToggle = document.querySelector('.menu-toggle');
 const navLinks = document.querySelector('.header-icons');
 
@@ -7,31 +9,49 @@ menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
 
-// Slideshow
+// Slideshow existente
 const slides = document.querySelectorAll('.about-img-container');
 let currentSlide = 0;
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
         if (i === index) {
-            slide.classList.add('active'); // Exibe a imagem ativa
-            slide.style.display = 'block'; // Garante que a imagem está visível
-            slide.style.opacity = 1; // Torna a imagem ativa visível
+            slide.classList.add('active');
+            slide.style.display = 'block';
+            slide.style.opacity = 1;
         } else {
-            slide.style.opacity = 0; // Oculta a imagem
+            slide.style.opacity = 0;
             setTimeout(() => {
-                slide.style.display = 'none'; // Esconde a imagem depois da transição
-                slide.classList.remove('active'); // Remove a classe de ativo
-            }, 3000); // Tempo da transição deve ser o mesmo que no CSS
+                slide.style.display = 'none';
+                slide.classList.remove('active');
+            }, 3000);
         }
     });
 }
 
 // Muda para o próximo slide a cada 5 segundos
 setInterval(() => {
-    currentSlide = (currentSlide + 1) % slides.length; // Muda para o próximo slide
+    currentSlide = (currentSlide + 1) % slides.length;
     showSlide(currentSlide);
 }, 5000);
 
 // Mostra o primeiro slide ao carregar
 showSlide(currentSlide);
+
+// Novo carrossel Swiper
+var swiper = new Swiper(".swiper-container", {
+    cssMode: true,
+    loop: true,
+    autoplay: {
+        delay: 5000, // Tempo de transição entre os slides (em milissegundos)
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    keyboard: true,
+});
+
